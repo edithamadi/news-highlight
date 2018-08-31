cb1c62c4138a40b207acf1cafd24e6bbd8cd78aa
Edith <you@example.com>
2018-08-31 16:46:44 +0300

    created configuration classes

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask #import flask class from flask module
+from .config import DevConfig
 
 # Initializing application
 app = Flask(__name__)
+
+#setting up configuration
+app.config.from_object(DevConfig)
 
 from app import views #import views file from app folder
