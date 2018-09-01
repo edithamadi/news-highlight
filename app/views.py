922a3389f5628a91dce5a896246aefa8d43e576f
Edith <you@example.com>
2018-09-01 11:43:05 +0300

    stored API KEY

--- app/views.py
+++ app/views.py
@@ -5,17 +5,11 @@ from app import app#import app instance from app folder
 @app.route('/')
 def index():
 
-    '''
-    View root page function that returns the index page and its data
-    '''
-   title = 'Welcome to The Home of the best news stories'
+    title = 'Welcome to The Home of the best news stories'
     return render_template('index.html', title = title)
 
 @app.route('/article/<int:article_id>')#dynamic route
 def article(article_id):
-
-    '''
-    View root page function that returns the index page and its data
-    '''
-    return render_template('article.html',id = article_id)   
+    
+        return render_template('article.html',id = article_id)   
 
