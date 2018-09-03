acb76f7cb8a020806343bfb7d3cfaa94334387f8
Edith <you@example.com>
2018-09-03 10:05:58 +0300

    updated file

--- app/models/article.py
+++ app/models/article.py
@@ -2,9 +2,9 @@ class Article:
     '''
     Article class to define News Articles Objects
     '''
-    def __init__(self,author,title,description,url,urlToImage,publishedAt):
+    def __init__(self,name,author,description,url,urlToImage,publishedAt):
+        self.name=name
         self.author = author
-        self.title = title
         self.description = description
         self.url = url
         self.urlToImage = urlToImage
