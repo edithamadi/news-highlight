1297183d65e1163fb1b367e45e102b3e5ee040e7
Edith <you@example.com>
2018-09-04 12:35:38 +0300

    added search functionality

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,9 @@
-from flask import render_template,redirect,url_for,#import render template function from flask
-from app import app#import app instance from app folder
-from .request import get_newarticle,get_sources, get_topheadlines, get_everything, search_source
+from flask import render_template,redirect,url_for#import render template function from flask
+from . import main#import app instance from app folder
+from ..request import get_newarticle,get_sources, get_topheadlines, get_everything, search_source
 
 # Views
-@app.route('/')
+@main.route('/')
 def index():
 
     #Getting news sources
@@ -25,61 +25,61 @@ else:
     return render_template('index.html', title=title, general = general_newsource, technology = technology_newsource, entertainment = entertainment_newsource, sports = sports_newsource, business= business_newsource, science = science_newsource)
 
 
-@app.route('/article/<int:article_id>')#dynamic route
+@main.route('/article/<int:article_id>')#dynamic route
 def article(article_id):
     
         return render_template('article.html',id = article_id)   
 
 
 #Getting articles
-    article_source = get_source(source_id,per_page)
 
+    article_source = get_article(source_id,per_page)
     title = f'{source_id} | All Articles'
-    search_source = request.args.get('article_query')
-    if search_source:
-        return redirect(url_for('.search',article_name=search_source))
+    search_article = request.args.get('article_query')
+    if search_article:
+        return redirect(url_for('.search',article_name=search_article))
     else:
-        return render_template('article.html', title=title, name = source_id, articles = article_source)
+        return render_template('article.html', title=title, name = source_id, article = article_source)
 
-@app.route('/topheadlines&<int:per_page>')
+@main.route('/topheadline&<int:per_page>')
 def topheadlines(per_page):
     '''
     Function that returns top headline articles
     '''
 
-    topheadlines_news = get_topheadlines(per_page)
+    topheadline_news = get_topheadline(per_page)
 
     title = 'Top Headlines'
 
-    search_source = request.args.get('article_query')
-    if search_source:
-        return redirect(url_for('.search',article_name=search_source))
+    search_article = request.args.get('article_query')
+    if search_article:
+        return redirect(url_for('.search',article_name=search_article))
     else:
-        return render_template('topheadlines.html', title=title, name = 'Top Headlines', articles = topheadlines_news)
+        return render_template('topheadline.html', title=title, name = 'Top Headlines', articles = topheadline_news)
 
-@app.route('/everything&<int:per_page>')
+@main.route('/everything&<int:per_page>')
 def all_news(per_page):
     '''
-    Function used to retirn all news articles
+    Function used to return all news articles
     '''
 
     everything_news = get_everything(per_page)
 
     title = 'All News'
     search_source = request.args.get('article_query')
-    if search_source:
-        return redirect(url_for('.search',article_name=search_source))
+    if search_article:
+        return redirect(url_for('.search',article_name=search_article))
     else:
-        return render_template('everything.html', title=title, name = 'All News', articles = everything_news)
+        return render_template('everything.html', title=title, name = 'All News', article = everything_news)
 
-@app.route('/search/<article_name>')
+@main.route('/search/<article_name>')
 def search(article_name):
     '''
     View function to display search results
     '''
-    source_name_list = source_name.split(" ")
-    source_name_format = "+".join(source_name_list)
-    searched_sources = search_source(source_name_format)
-    title = f'search results for {source_name}'
+    article_name_list = article_name.split(" ")
+    article_name_format = "+".join(article_name_list)
+    searched_articles = search_article(articlee_name_format)
+    title = f'search results for {article_name}'
 
-    return render_template('search.html', articles = searched_sources)
+    return render_template('search.html', article = searched_articles)
