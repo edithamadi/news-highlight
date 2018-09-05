260922ebe70e368cfb74f2e63cab0f2748dda55e
Edith <you@example.com>
2018-09-05 14:33:41 +0300

    created create_app function

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,28 @@
 from flask import Flask #import flask class from flask module
-from .config import DevConfig
+from flask_bootstrap import Bootstrap
+from config import config_options
 
+bootstrap = Bootstrap()
+
+def create_app(config_name):
+    
 # Initializing application
-app = Flask(__name__,instance_relative_config = True)
+app = Flask(__name__)
 
-#setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
+# Creating the app configurations
+    app.config.from_object(config_options[config_name])
 
-from app import views #import views file from app folder
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+
+# Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    # setting config
+    from .request import configure_request
+    configure_request(app)
+
+    # Will add the views and forms
+
+    return app
